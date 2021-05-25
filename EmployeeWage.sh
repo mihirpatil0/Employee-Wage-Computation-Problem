286ded1bb3afc40cfe9e640b95b5094d5821dcf0
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-25 17:19:49 +0530

    Attendance checker

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
 
+isPresent=1
+Random=$((RANDOM%2))
+
+if [ $Random -eq $isPresent ]
+then
+	echo "Employee is Full Time Present"
+else
+	echo "Employee is Absent"
+fi
 
