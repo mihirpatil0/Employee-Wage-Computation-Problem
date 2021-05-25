1b93cbc626ffa2c6dfdc420eb3afe43fbc0e3c6a
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-25 18:59:33 +0530

    Used Switch Case To Solve The Problem

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
-isAbsent=0
-isPresent=1
-isPartPresent=2
+
 WagePerHr=20
-FullDayHr=8
-PartDayHr=4
 Random=$((RANDOM%3))
 
+case $Random in
 
-if [ $Random -eq $isPresent ]
-then
-	echo "Employee is Full Time Present"
-	EmpWageCalc=$(($WagePerHr*$FullDayHr))	
-	echo "Daily Wage of an Full Time Present Employee is : $EmpWageCalc"
-elif [ $Random -eq $isPartPresent ]
-then
-	echo "Employee is Part Time Present"
-	EmpWageCalc=$(($WagePerHr*$PartDayHr))
-	echo "Daily Wage of an Part Time Present Employee is : $EmpWageCalc"
-
-else
-	echo "Employee is Absent"
-fi
-
+	1) echo "Employee is Full Time Present"
+	   DayHr=8 ;;
+	2) echo "Employee is Part Time Present"
+	   DayHr=4 ;;
+	0) echo "Employee is Absent"
+	   DayHr=0 ;;
+esac
+WageCalc=$(($WagePerHr*$DayHr))
+echo "The Employee's Wage is : $WageCalc"
 
 
 
