0490964668faf118b9e96c456e066676462f3b31
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-25 20:48:24 +0530

    Addted Calculate wage if 100 hr breach or 20 days complited

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
 
+WorkHr=0
 Month=20
 WagePerHr=20
 MonthWage=0
 
 for (( i=1; i<=$Month; i++ ))
 do
-	Random=$((RANDOM%3))
-	case $Random in
+        if [ $WorkHr -lt 100 ]
+        then
+                Random=$((RANDOM%3))
+                case $Random in
 
-		1) #echo "Employee is Full Time Present"
-	   		DayHr=8
-			WageCalc=$(($WagePerHr*$DayHr))
-			MonthWage=$(($MonthWage+$WageCalc)) ;;
-		2) #echo "Employee is Part Time Present"
-	   		DayHr=4
-			WageCalc=$(($WagePerHr*$DayHr))
-			MonthWage=$(($MonthWage+$WageCalc)) ;;
-		0) #echo "Employee is Absent"
-	   		DayHr=0
-			WageCalc=$(($WagePerHr*$DayHr))
-			MonthWage=$(($MonthWage+$WageCalc)) ;;
-	esac
+                        1) #echo "Employee is Full Time Present"
+                                DayHr=8
+                                WageCalc=$(($WagePerHr*$DayHr))
+                                MonthWage=$(($MonthWage+$WageCalc))
+                                WorkHr=$(($WorkHr+$DayHr)) ;;
+                        2) #echo "Employee is Part Time Present"
+                                DayHr=4
+                                WageCalc=$(($WagePerHr*$DayHr))
+                                MonthWage=$(($MonthWage+$WageCalc))
+                                WorkHr=$(($WorkHr+$DayHr)) ;;
+                        0) #echo "Employee is Absent"
+                                DayHr=0
+
+                esac
+        else
+                break;
+        fi
 done
 #echo "The Employee's Daily Wage is : $WageCalc"
-echo "The Employee Monthly Wage is : $MonthWage"
+#echo "The Employee Monthly Wage is : $MonthWage"
+echo "The Employee Monthly wage is $MonthWage Rupee for $WorkHr Hr in $(($i-1)) days"
+
