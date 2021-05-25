fe16305ee059782f9f92eb67591f4c479b218524
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-25 17:49:35 +0530

    Daily wage calculation

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,12 +2,22 @@
 echo "WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM"
 
 isPresent=1
+WagePerHr=20
+FullDayHr=8
 Random=$((RANDOM%2))
+
 
 if [ $Random -eq $isPresent ]
 then
 	echo "Employee is Full Time Present"
+	EmpWageCalc=$(($WagePerHr*$FullDayHr))	
+	echo "Daily Wage of an Full Time Present Employee is : $EmpWageCalc"
 else
 	echo "Employee is Absent"
 fi
+
+
+
+
+
 
